6e01041c3e577cc74d606e0d2866e37e21018aef
Sweeta-Selvan <sweetaselvan@gmail.com>
2025-11-08 12:06:21 +0530

    Updated hackbio scripts

---
+++ numpy_1/stage_one_hackbio.py
@@ -0,0 +1,39 @@
+#calculate hamming distance
+'''a = ['SweetaS', 'Suvitha']
+sum = 0
+for i in range(len(a)):
+    for j in range(i+1, len(a)):
+        sum = 0
+        for m in range(len(a[i])):
+            if a[i][m] != a[j][m]:
+                    sum = sum+1
+        print(sum)'''
+#translate DNA to protein
+def translate(dna):
+     codon_table = {
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+    }
+     protein = ""
+     for i in range(0, len(dna),3):
+          protein = protein + codon_table[dna[i:i+3]]
+
+     return protein
+print(translate('GGTACCGTACGTCGATCGGAC'))
+          
+
+              
